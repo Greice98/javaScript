40c46ac251437ed0860380a66e47cfe77286e321
Greice98 <gracykellysilva.98@gmail.com>
2021-11-04 21:49:28 -0300

    aula 04.11

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+//SWITCH
+const nota = 6;
+
+switch(nota) {
+    case nota == 4:
+    console.log("Reprovado!");
+    break;
+    case nota == 6:
+
+    console.log("Recuperação");
+    break;
+ default:
+     
+        console.log("Nota acima da Média");
+        break;
+}
+
+
+console.clear();
+
+const b = 10;
+if (b == 10) {
+    console.log("Os valores são iguais");
+}else if (a > 10){
+console.log("A variavel A não é igual a 10 e não é maior que 10");
+}else {
+    console.log("Os valores são diferente")
+}
+
+let c = b== 16? "É igual a 10" : "São diferentes"
+console.log(c)
+
+let resultado = 3 == 4 ? "3 é diferente de 4" : "3 é igual a 4"
+console.log(resultado)
